d7bd8dbcc0f388cac687a98447f76b357a68e561
diego <diegoaguilar221202@gmail.com>
2023-10-07 19:12:48 -0400

    implementacion de informacion de perfil

--- src/components/CustomDrawer.jsx
+++ src/components/CustomDrawer.jsx
@@ -1,6 +1,12 @@
-import { Text, View, TouchableOpacity } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+import {
+    Avatar,
+    Title,
+    Caption,
+    Text
+} from 'react-native-paper';
 
 import { Ionicons } from '@expo/vector-icons';
 
@@ -8,6 +14,22 @@ const CustomDrawer = (props) => {
     return (
         <View style={{flex: 1}}>
             <DrawerContentScrollView {...props} >
+                <View style={{paddingLeft: 20,marginBottom:20}}>
+                    <View style={{flexDirection:'row',marginTop: 15}}>
+                        <Avatar.Image 
+                            source={require('../../assets/img/profile.png')}
+                            size={60}
+                        />
+                        <View style={{marginLeft:15, flexDirection:'column'}}>
+                            <Title style={{ fontSize: 16, marginTop: 3, fontWeight: 'bold' }}>
+                                Diego Aguilar
+                            </Title>
+                            <Caption style={{ fontSize: 14, lineHeight: 14 }}>
+                                C.I.: 29831184
+                            </Caption>
+                        </View>
+                    </View>
+                </View>
                 <View>
                     <DrawerItemList {...props} />
                 </View>

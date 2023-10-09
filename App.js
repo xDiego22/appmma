4ec472d21bc932bde09483f045c74de4291a6469
diego <diegoaguilar221202@gmail.com>
2023-10-08 20:54:12 -0400

    reajuste de lineas

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-
-// import Navegacion from './src/components/Navegacion.jsx';
-// import Login from './src/views/Login.jsx';
 import AuthNavigator from './src/navigation/AuthNavigator.jsx';
-
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-
         <AuthNavigator/>
       </NavigationContainer>
-      {/* <Navegacion/> */}
     </>
   ); 
 }

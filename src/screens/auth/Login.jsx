7ca30cf4d9f474adda70883217142b48d37bbbe9
diego <diegoaguilar221202@gmail.com>
2023-10-17 02:46:56 -0400

    validaciones de campos de entrada

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -5,11 +5,38 @@ import InputField from '../../components/InputField.jsx';
 import Captcha from '../../components/Captcha.jsx';
 import {AuthContext} from '../../context/AuthContext.jsx';
 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 const Login = ({ navigation }) => {
+
     const [cedula, setCedula] = useState(null);
     const [password, setPassword] = useState(null);
+    const [isCedulaValid, setIsCedulaValid] = useState(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+    const { login } = useContext(AuthContext);
     
-    const {login} = useContext(AuthContext);
+    const cedulaRegex = /^[0-9]{7,8}$/;
+    const passwordRegex = /^[A-Za-z0-9ñÑ_.@$!%*?&#\/\b-]{6,70}$/;
+    
+    const handleCedulaChange = (text) => {
+        setCedula(text);
+        setIsCedulaValid(cedulaRegex.test(text));
+    };
+
+    const handlePasswordChange = (text) => {
+        setPassword(text);
+        setIsPasswordValid(passwordRegex.test(text));
+    };
+
+    const handleLogin = () => {
+        if (isCedulaValid && isPasswordValid) {
+            login(cedula, password);
+        } else {
+            alert('Por favor, verifica los campos de entrada.');
+        }
+    };
+
     return (
         <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
             <View style={{paddingHorizontal: 20}}>
@@ -17,12 +44,12 @@ const Login = ({ navigation }) => {
                 <Text
                     style={{
                     textAlign:'center',
-                    fontSize: 28,
+                    fontSize: 30,
                     fontWeight: '500',
                     color: '#333',
                     marginBottom: 70,
                     }}>
-                    Bienvenido! 
+                    Bienvenido! <MaterialCommunityIcons name="hand-wave-outline" size={28} color="black" />
                 </Text>
 
                 <InputField
@@ -37,7 +64,8 @@ const Login = ({ navigation }) => {
                     }
                     keyboardType='numeric'
                     value={cedula}
-                    onChangeText = {text => setCedula(text)}
+                    maxLength={8}
+                    onChangeText = {handleCedulaChange}
                 />
 
                 <InputField
@@ -52,17 +80,25 @@ const Login = ({ navigation }) => {
                     }
                     inputType="password"
                     value={password}
-                    onChangeText = {text => setPassword(text)}
+                    maxLength={25}
+                    onChangeText = {handlePasswordChange}
                 />
+
+                {!isCedulaValid && (
+                    <Text style={{ color: 'red' }}>Cedula no válida</Text>
+                )}
+                {!isPasswordValid && (
+                    <Text style={{ color: 'red' }}>Contraseña no válida</Text>
+                )}
             
                 <TouchableOpacity
-                    onPress={() => login(cedula,password)}
+                    onPress={handleLogin}
                     style={{
                         backgroundColor: '#FF5C5C',
                         padding: 20,
                         borderRadius: 10,
                         marginBottom: 30,
-                        marginTop: 20
+                        marginTop: 40
                     }}>
                     <Text
                         style={{

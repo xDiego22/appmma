8daedbe99c422daa6bfc0497899cfd5ce6556c44
diego <diegoaguilar221202@gmail.com>
2023-10-17 01:46:54 -0400

    se implemento componente de pantalla de carga

--- src/navigation/AppNav.jsx
+++ src/navigation/AppNav.jsx
@@ -1,10 +1,9 @@
 import React, { useContext } from 'react'
-import { View } from 'react-native';
-import { ActivityIndicator } from 'react-native-paper'
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./AuthNavigator.jsx";
 import DrawerNavigator from './DrawerNavigator.jsx'
 import { AuthContext } from '../context/AuthContext.jsx';
+import ChargeIndicator from '../components/ChargeIndicator.jsx';
 
 const AppNav = () => {
 
@@ -12,9 +11,7 @@ const AppNav = () => {
 
   if (isLoading) {
     return (
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <ActivityIndicator size={'large'} animating={true} color={ 'blue' } /> 
-      </View>
+      <ChargeIndicator/>
     )
   }
   return (

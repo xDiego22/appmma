4b7fdd908fcc20b179d4e212c477e46d6fedc848
Cebasmb <cirezeduardo10@gmail.com>
2023-10-17 04:08:49 -0400

    navegacion a traves de las nuevas vistas

--- src/navigation/DrawerNavigator.jsx
+++ src/navigation/DrawerNavigator.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home.jsx';
-import Clubes from '../screens/Clubes.jsx';
-import Inscripcion from '../screens/Inscripcion.jsx';
+import historial from '../screens/historial.jsx';
+import ReporteEvento from '../screens/Reporteevento.jsx';
+import ReporteAtleta from '../screens/reporteatleta.jsx';
+import ReporteResultado from '../screens/ReporteResultado.jsx';
 import CustomDrawer from '../components/CustomDrawer.jsx'
 import {MaterialIcons} from '@expo/vector-icons/';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -11,8 +13,10 @@ import { StatusBar } from 'expo-status-bar';
 
 const Drawer = createDrawerNavigator();
 const HomeIcon =({focused,color, size})=><MaterialIcons name="home" size={size} color={color} />
-const ClubesIcon = ({focused,color, size }) => <MaterialCommunityIcons name="boxing-glove" size={size} color={color} />
-const InscripcionIcon =({focused,color, size})=><Foundation name="clipboard-pencil" size={size} color={color} />
+const historialIcon = ({focused,color, size }) => <MaterialIcons name="history" size={24} color="black" />
+const ReporteeIcon =({focused,color, size})=><MaterialIcons name="event" size={24} color="black" />
+const ReporteaIcon =({focused,color, size})=><MaterialCommunityIcons name="karate" size={24} color="black" />
+const ReporterIcon =({focused,color, size})=><Foundation name="results" size={24} color="black" />
 
 
 const DrawerNavigator = () => {
@@ -46,18 +50,36 @@ const DrawerNavigator = () => {
                 />
                 <Drawer.Screen 
                     name="ClubesDrawer"
-                    component={Clubes}
+                    component={historial}
                     options={{
-                        drawerIcon: ClubesIcon,
-                        title: 'Clubes'
+                        drawerIcon: historialIcon,
+                        title:'Historial Atletas'
                     }}
                 />
                 <Drawer.Screen 
-                    name="IncripcionDrawer"
-                    component={Inscripcion}
+                    name="ReporteeveDrawer"
+                    component={ReporteEvento}
                     options={{
-                        drawerIcon: InscripcionIcon,
-                        title: 'Inscripcion'
+                        drawerIcon: ReporteeIcon,
+                        title: 'Reportes Eventos'
+                    }} 
+                />
+
+                <Drawer.Screen 
+                    name="Reporte1Drawer"
+                    component={ReporteAtleta}
+                    options={{
+                        drawerIcon: ReporteaIcon,
+                        title: 'Reportes Atletas'
+                    }} 
+                />
+
+                <Drawer.Screen 
+                    name="ReporteresDrawer"
+                    component={ReporteResultado}
+                    options={{
+                        drawerIcon: ReporterIcon,
+                        title: 'Reportes Resultados'
                     }} 
                 />
 
